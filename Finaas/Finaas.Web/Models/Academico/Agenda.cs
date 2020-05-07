46be3dfda01c6303aee1b24812f4e30d030250d3
flpuertacardona@hotmail.com <flpuertacardona@hotmail.com>
2020-05-07 21:10:46 +0200

    Creacion entidades admision

---
+++ Finaas/Finaas.Web/Models/Academico/Agenda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finaas.Web.Models.Academico
+{
+    public class Agenda
+    {
+
+    }
+}
---
+++ Finaas/Finaas.Web/Models/Academico/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finaas.Web.Models.Academico
+{
+    public class Curso
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public Profesor Profesor { get; set; }
+        public int Cupo { get; set; }
+        public int Disponible { get; set; }
+        public Programa Programa { get; set; }
+        public bool Estado => {?Disponible > 0 true : falsepublic override bool Equals(object obj)
+        {
+            return obj is Curso curso &&
+                   Estado == curso.Estado;
+        }
+    }
+    }
+}
