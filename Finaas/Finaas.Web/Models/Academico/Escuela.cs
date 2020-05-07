e48dc97b4c313a36464870478a9d8a858b592915
flpuertacardona@hotmail.com <flpuertacardona@hotmail.com>
2020-05-07 16:26:24 +0200

    Creacion Escuela y Empleado.

---
+++ Finaas/Finaas.Web/Models/Academico/Escuela.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finaas.Web.Models.Academico
+{
+    public class Escuela
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
