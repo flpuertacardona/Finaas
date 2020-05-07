49152799714b06f84a4bd340d2ec741a346d602f
flpuertacardona@hotmail.com <flpuertacardona@hotmail.com>
2020-05-07 20:19:11 +0200

    Creación entidades admision

--- Finaas/Finaas.Web/Models/Academico/Matricula.cs
+++ Finaas/Finaas.Web/Models/Academico/Matricula.cs
@@ -13,5 +13,10 @@ namespace Finaas.Web.Models.Academico
         public string Periodo { get; set; }
         public DateTime FechaAdmision { get; set; }
         public Programa Programa { get; set; }
+        public List<Asignatura> Asignaturas { get; set; }
+        public int CantidadCreditos { get; set; }
+        public float Descuento { get; set; }
+        public decimal ValorCredito { get; set; }
+        public decimal ValorDescuento{ get; set; }
     }
 }
