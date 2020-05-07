692ada3979f6ec58c9a805160e4f76cd1025307e
flpuertacardona@hotmail.com <flpuertacardona@hotmail.com>
2020-05-07 22:03:41 +0200

    Creaciòn entidad matriculas

--- Finaas/Finaas.Web/Models/Academico/Asistencia.cs
+++ Finaas/Finaas.Web/Models/Academico/Asistencia.cs
@@ -7,6 +7,11 @@ namespace Finaas.Web.Models.Academico
 {
     public class Asistencia
     {
-
+        public string Id { get; set; }
+        public Profesor Profesor { get; set; }
+        public string ProfesorId { get; set; }
+        public Curso Curso { get; set; }
+        public string CursoId { get; set; }
+        public List<Alumno> Alumnos { get; set; }
     }
 }
