b365e47ad4cbf05be4baf5634cebfd278b62ff65
flpuertacardona@hotmail.com <flpuertacardona@hotmail.com>
2020-05-07 18:47:55 +0200

    Creación entidades admision.

---
+++ Finaas/Finaas.Web/Models/Academico/Matricula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finaas.Web.Models.Academico
+{
+    public class Matricula
+    {
+        public string Id { get; set; }
+        public Alumno Alumno { get; set; }
+        public string AlumnoId { get; set; }
+        public string Periodo { get; set; }
+        public DateTime FechaAdmision { get; set; }
+        public Programa Programa { get; set; }
+        public TiposAdmision TipoAdmision { get; set; }
+    }
+}
